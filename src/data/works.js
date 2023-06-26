25795954099d89f746422f295b5996ad9d3140dc
akdeas oktanae <akdeaso@gmail.com>
2023-06-26 14:05:52 +0700

    update works

--- src/data/works.js
+++ src/data/works.js
@@ -1,21 +1,33 @@
 export default [
     {
-        title: 'Diabetes Prediction App',
-        imgUrl: 'https://cdn.discordapp.com/attachments/749769913668730992/1064433961167757332/image.png',
-        tech:['Flask', 'Tailwind CSS', 'Heroku'],
-        workUrl: 'https://github.com/akdeaso/diabetes-prediction'
+        title: 'JLPT Word Generator',
+        imgUrl: 'https://cdn.discordapp.com/attachments/751040199319879691/1122783507429732362/image.png',
+        tech:['React', 'Vite', 'Tailwind CSS'],
+        workUrl: 'https://github.com/akdeaso/jlpt-word-generator'
     },
     {
-        title: 'Second Hand',
-        imgUrl: 'https://cdn.discordapp.com/attachments/749769913668730992/1064434084736155708/image.png',
-        tech:['React Native', 'JavaScript'],
-        workUrl: 'https://github.com/MauL08/SecondHand'
+        title: 'Indonesia Yanwoo',
+        imgUrl: 'https://cdn.discordapp.com/attachments/751040199319879691/1122784211078750248/image.png',
+        tech:['Wordpress'],
+        workUrl: 'https://indonesiayanwoo.com/'
     },
     {
         title: 'My Desktop',
         imgUrl: 'https://cdn.discordapp.com/attachments/749769913668730992/1064434263694528552/image.png',
         tech:['React', 'Vite', 'Express', 'MongoDB', 'Redis', 'Neo4j', 'Postgresql'],
         workUrl: 'https://my-desktop-tbd.netlify.app/login'
+    },
+    {
+        title: 'Second Hand',
+        imgUrl: 'https://cdn.discordapp.com/attachments/751040199319879691/1122783109021192262/Second_Hand.png',
+        tech:['React Native', 'JavaScript'],
+        workUrl: 'https://github.com/MauL08/SecondHand'
+    },
+    {
+        title: 'Diabetes Prediction App',
+        imgUrl: 'https://cdn.discordapp.com/attachments/749769913668730992/1064433961167757332/image.png',
+        tech:['Flask', 'Tailwind CSS', 'Heroku'],
+        workUrl: 'https://github.com/akdeaso/diabetes-prediction'
     },
     {
         title: 'BIONIX 2020',
