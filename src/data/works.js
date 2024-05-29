e4d364031af402cd06aa6d324587c7df92f71ae1
AKDEAS\akdea <akdeaso@gmail.com>
2024-05-29 12:43:09 +0700

    fix: move img to local

--- src/data/works.js
+++ src/data/works.js
@@ -1,10 +1,16 @@
+import stp from "../img/stp.png";
+import pos from "../img/pos.png";
+import indonesiayanwoo from "../img/indonesiayanwoo.png";
+import mydesktop from "../img/mydesktop.png";
+import secondhand from "../img/secondhand.png";
+import bionix from "../img/bionix.png";
+
 export default [
   {
     title: "STP Otomotif ITS",
-    date: "October 2023 - Recent",
+    date: "October 2023 - Present",
     desc: "A website built to showcase the company profile of STP Otomotif ITS along with the latest news in it",
-    imgUrl:
-      "https://cdn.discordapp.com/attachments/1230822676000538625/1230826135974055946/image.png?ex=6634bb2a&is=6622462a&hm=9ace06960597adf42ffa0932de6fd8387521ce60558c519437611bb30ae492b8&",
+    imgUrl: stp,
     tech: ["Wordpress", "Visual Composer"],
     linkUrl: "",
     demoUrl: "https://www.its.ac.id/stp/pusat-otomotif/",
@@ -13,8 +19,7 @@ export default [
     title: "Fullstack Food Store",
     date: "July - September 2023",
     desc: "A website specifically designed for the purpose of selling food products",
-    imgUrl:
-      "https://cdn.discordapp.com/attachments/1230822676000538625/1230822710565670942/screencapture-localhost-3001-2023-09-08-14_58_56.png?ex=6634b7f9&is=662242f9&hm=8cdc3937e818314b61e3526bb91c336273e4eceeec6a3ee901e5b199cd73b6fc&",
+    imgUrl: pos,
     tech: ["React", "Tailwind CSS", "Node JS & Express JS", "MongoDB"],
     linkUrl: "https://github.com/akdeaso/pos-client",
     demoUrl: "https://pos-client-sigma.vercel.app/",
@@ -23,8 +28,7 @@ export default [
     title: "Indonesia Yanwoo",
     date: "February - June 2023",
     desc: "A website to increase sales conversions for edible bird nest",
-    imgUrl:
-      "https://cdn.discordapp.com/attachments/751040199319879691/1122784211078750248/image.png?ex=662f4648&is=661cd148&hm=b68917fa32fa74d48eee18c39f3c36a198d363aaffb4c21ad99d7f2075432eff&",
+    imgUrl: indonesiayanwoo,
     tech: ["Wordpress"],
     linkUrl: "",
     demoUrl: "https://indonesiayanwoo.com/",
@@ -33,8 +37,7 @@ export default [
     title: "My Desktop",
     date: "November – Desember 2022",
     desc: "A website for showcasing desktop setup",
-    imgUrl:
-      "https://camo.githubusercontent.com/40b94cff9423ce737ed40063745a7b2702a1c076100b2c2bcc8e1f94a47a2059/68747470733a2f2f64726976652e676f6f676c652e636f6d2f75633f6578706f72743d766965772669643d31595572504d556d4d6c70324d615053656e384a78424f654f434869704937484f",
+    imgUrl: mydesktop,
     tech: ["React", "Vite", "Material UI"],
     linkUrl: "https://github.com/akdeaso/tbd-fp-fe",
     demoUrl: "https://my-desktop-tbd.netlify.app/login",
@@ -43,8 +46,7 @@ export default [
     title: "Second Hand",
     date: "June – July 2022",
     desc: "Mobile app for buying and selling preloved second-hand goods",
-    imgUrl:
-      "https://cdn.discordapp.com/attachments/751040199319879691/1122783109021192262/Second_Hand.png?ex=662f4541&is=661cd041&hm=a924dba767f9dcf75bd0ea420456d75e92c8a495c92a1fbad3b050eb69f0ffd2&",
+    imgUrl: secondhand,
     tech: ["React Native", "JavaScript"],
     linkUrl: "https://github.com/MauL08/SecondHand",
     demoUrl: "",
@@ -61,8 +63,7 @@ export default [
     title: "BIONIX 2020",
     date: "September – December 2020",
     desc: "Website for one of the competition branches at the biggest ITS information systems event",
-    imgUrl:
-      "https://cdn.discordapp.com/attachments/1230822676000538625/1230824100193243188/image.png?ex=6634b945&is=66224445&hm=86a88547a84023c1caa3df65ce7472a6e41d4d860fe64c6091e99d8ebff9de88&",
+    imgUrl: bionix,
     tech: ["HTML", "CSS", "JS", "Bootstrap"],
     linkUrl: "",
     demoUrl: "https://bionix-ise-2020.netlify.app/",
